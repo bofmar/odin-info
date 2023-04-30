56c7b48a4f16183fdb9fd87d0e87ae4878cfcd4d
bofmar <bofmar@outlook.com>
2023-04-30 20:14:59 +0300

    Build the dist

---
+++ dist/server.js
@@ -0,0 +1,33 @@
+import http from 'http';
+import fs from 'fs';
+const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    let path = './src/pages/';
+    let statusCode = 200;
+    switch (req.url) {
+        case '/':
+            path += 'index.html';
+            break;
+        case '/about':
+            path += 'about.html';
+            break;
+        case '/contact-me':
+            path += 'contact-me.html';
+            break;
+        default:
+            statusCode = 404;
+            path += '404.html';
+            break;
+    }
+    res.statusCode = statusCode;
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        }
+        res.write(data);
+        res.end();
+    });
+});
+server.listen(3000, () => console.log('Server started'));
+//# sourceMappingURL=server.js.map
