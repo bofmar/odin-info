295a2769dfb9d319189b392f2878b25789db07d5
bofmar <bofmar@outlook.com>
2023-04-30 19:51:15 +0300

    Make the server always return the index.html

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,21 @@
 import http from 'http';
+import fs from 'fs';
+
 
 const server = http.createServer((req, res) => {
-  res.end('Hi!');
+  res.setHeader('Content-Type', 'text/html');
+  //let path = './src/pages/';
+
+  res.statusCode = 200;
+
+  fs.readFile('./src/pages/index.html', (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    }
+    res.write(data);
+    res.end();
+  })
 });
 
 server.listen(3000, () => console.log('Server started'));
