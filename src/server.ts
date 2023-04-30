020f60d88b837ac36dd08b51b770791e02a71af6
bofmar <bofmar@outlook.com>
2023-04-30 20:10:06 +0300

    Add routing

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,26 @@ import fs from 'fs';
 
 const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/html');
-  //let path = './src/pages/';
+  let path = './src/pages/';
+
+  switch (req.url) {
+    case '/':
+      path += 'index.html';
+      break;
+    case '/about':
+      path += 'about.html';
+      break;
+    case '/contact-me':
+      path += 'contact-me.html';
+      break;
+    default:
+      path += '404.html';
+      break;
+  }
 
   res.statusCode = 200;
 
-  fs.readFile('./src/pages/index.html', (err, data) => {
+  fs.readFile(path, (err, data) => {
     if (err) {
       console.log(err);
       res.end();
